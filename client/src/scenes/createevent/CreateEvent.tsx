f81cbb0afae8186cac42a6d16d1b37835ad07561
Nasra Hussein <nh3@live.co.uk>
2023-12-09 19:48:13 +0000

    changes

--- client/src/scenes/createevent/CreateEvent.tsx
+++ client/src/scenes/createevent/CreateEvent.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import GreySquiggle from "../../assets/images/GreySquiggle.jpg";
+import axios from 'axios'
 
 const CreateEvent = () => {
   const location = useLocation();
--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -6,20 +6,31 @@ const session = require('express-session');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 
-// function to start events table creation in the database
-async function createEventInDatabase(eventData) {
+// Function to start events table creation in the database
+async function createEventInDatabase(userId, eventData) {
   try {
-    const [result] = await db.execute('INSERT INTO events (ID, event_name, event_type, event_decor, date, location, catering_type, status, event_confirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [eventData.ID, eventData.event_name, eventData.event_type, eventData.event_decor, eventData.date, eventData.location, eventData.catering_type, eventData.status, eventData.event_confirmed]);
+    // Insert event data into the database, including the user_id
+    const [result] = await db.execute(
+      'INSERT INTO Events (user_id, event_name, event_theme, event_date, event_time, location, catering_type, decoration_style, event_status, event_confirmed, location_new) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [userId, eventData.event_name, eventData.event_theme, eventData.event_date, eventData.event_time, eventData.location, eventData.catering_type, eventData.decoration_style, 'PLANNING IN PROGRESS', 'NOT CONFIRMED', eventData.location_new]
+    );
+
+    // Fetch the created event from the database
+    const [createdEvent] = await db.execute('SELECT * FROM Events WHERE ID = ?', [result.insertId]);
+
     return {
-      id: result.insertId,
-      event_name: eventData.event_name,
-      event_type: eventData.event_type,
-      event_decor: eventData.event_decor,
-      date: eventData.date,
-      location: eventData.location,
-      catering_type: eventData.catering_type,
-      status: eventData.status,
-      event_confirmed: eventData.event_confirmed,
+      id: createdEvent[0].ID,
+      user_id: createdEvent[0].user_id,
+      event_name: createdEvent[0].event_name,
+      event_theme: createdEvent[0].event_theme,
+      event_date: createdEvent[0].event_date,
+      event_time: createdEvent[0].event_time,
+      location: createdEvent[0].location,
+      catering_type: createdEvent[0].catering_type,
+      decoration_style: createdEvent[0].decoration_style,
+      event_status: createdEvent[0].event_status,
+      event_confirmed: createdEvent[0].event_confirmed,
+      location_new: createdEvent[0].location_new,
     };
   } catch (error) {
     throw error;
@@ -28,52 +39,26 @@ async function createEventInDatabase(eventData) {
 
 
 // Select the type of event
-router.get('/choose-event', (req, res) => {
+router.get('/choose-event', isAuthenticated, (req, res) => {
   const eventTypes = ['Bridal Shower', 'Birthday', 'Kids Birthday', 'Baby Shower'];
 
   res.json({ eventTypes });
 });
 
 
-//Choose Bridal Shower subtype (Bridal Day Party or Bridal Night Party)
-router.get('/choose-event/bridal-shower', (req, res) => {
-  const bridalShowerSubtypes = ['Bridal Day Party', 'Bridal Night Party'];
-
-  res.json({ bridalShowerSubtypes });
-});
-
-// Choose Birthday subtype (Guys Night or Ladies Night)
-router.get('/choose-event/birthday', (req, res) => {
-  const birthdaySubtypes = ['Guys Night', 'Ladies Night'];
-
-  res.json({ birthdaySubtypes });
-});
-
-// Choose Kids Birthday subtype (Girls Birthday Party, Boys Birthday Party, or No Gender Birthday Party)
-router.get('/choose-event/kids-birthday', (req, res) => {
-  const kidsBirthdaySubtypes = ['Girls Birthday Party', 'Boys Birthday Party', 'No Gender Birthday Party'];
-
-  res.json({ kidsBirthdaySubtypes });
-});
-
-// Choose Baby Shower subtype (Boys Baby Shower, Girls Baby Shower, or No Gender Baby Shower)
-router.get('/choose-event/baby-shower', (req, res) => {
-  const babyShowerSubtypes = ['Boys Baby Shower', 'Girls Baby Shower', 'No Gender Baby Shower'];
-
-  res.json({ babyShowerSubtypes });
-});
-
 // Choose the location type (Grand Venue, Bar, Garden, Intimate Venue)
-router.get('/choose-location', (req, res) => {
+router.get('/choose-location', isAuthenticated, (req, res) => {
   const locationTypes = ['Grand Venue', 'Bar', 'Garden', 'Intimate Venue'];
 
   res.json({ locationTypes });
 });
 
+
 // Choose the date and time for the event
-router.get('/choose-date-time', (req, res) => {
+router.get('/choose-date-time', isAuthenticated, (req, res) => {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
+
 
   // Generate date options for the entire year
   const dateOptions = [];
@@ -92,7 +77,7 @@ router.get('/choose-date-time', (req, res) => {
 
 
 // Choose the type of catering (Cold Food, Hot Food, Soft Drinks, Cakes, Alcohol and Mocktails, Sweets/Pastries)
-router.get('/choose-catering', (req, res) => {
+router.get('/choose-catering', isAuthenticated, (req, res) => {
   const cateringTypes = ['Cold Food', 'Hot Food', 'Soft Drinks', 'Cakes', 'Alcohol and Mocktails', 'Sweets/Pastries'];
 
   res.json({ cateringTypes });
@@ -100,12 +85,14 @@ router.get('/choose-catering', (req, res) => {
 
 
 // Process the confirmed booking and display a thank you message
-router.post('/confirm-booking', async (req, res) => {
+router.post('/confirm-booking', isAuthenticated, async (req, res) => {
   try {
     const eventData = req.body;
+    // Get the user ID from the session
+    const userId = req.session.user.user_id;
 
-    // Call the createEventInDatabase method
-    const createdEvent = await createEventInDatabase(eventData);
+    // Call the createEventInDatabase method with the user ID
+    const createdEvent = await createEventInDatabase(userId, eventData);
 
     res.status(201).json({ message: 'YAY! Thank you for booking! Your event has been confirmed.', event: createdEvent });
   } catch (error) {
@@ -113,6 +100,7 @@ router.post('/confirm-booking', async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
 
 
 
