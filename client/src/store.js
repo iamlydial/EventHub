f11447af8ac5b20361c14f8d2e63be8dc45f89f2
iamlydial <lydia.lavecchia@gmail.com>
2023-12-09 20:22:50 +0100

    1. completed signup and login functions with redirectionsrespectively to login once the user has finished signup; and to homepage when the user has logged in

--- client/src/store.js
+++ client/src/store.js
@@ -1,29 +1,19 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const greetingReducer = (state, action) => {
-    if (action.type === 'changeGreeting') {
-        return {greeting: action.payload}
-    } else {
-        return state;
-    }
-}
+  if (action.type === "changeGreeting") {
+    return { greeting: action.payload };
+  } else {
+    return state;
+  }
+};
 
-export const store =createStore(greetingReducer, {greeting: 'Welcome'})
+export const store = createStore(greetingReducer, { greeting: "Welcome" });
 
-Component.js
-const greeting = useSelector(s => s.greeting);
+Component.js;
+const greeting = useSelector((s) => s.greeting);
 const dispatch = useDispatch();
 
 function whenButtonPressed() {
-    dispatchEvent({type: "changeGreeting", paylod: "Hi Mina"})
-
+  dispatchEvent({ type: "changeGreeting", paylod: "Hi Mina" });
 }
-
-
-
-return 
-<>
-
-<buttonclassName
-
-
