6b9cf1a0c29e7da73366ddeca19b7b7f91e55de9
MinkaAnsa <ansaminka@gmail.com>
2023-12-13 21:35:48 +0000

    Changed link for visibility

--- client/src/scenes/home/Home.tsx
+++ client/src/scenes/home/Home.tsx
@@ -30,7 +30,7 @@ const Home: React.FC<Props> = () => {
 
   return (
     <div className="mt-20">
-      <div className="bg-[url('assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg')] bg-fixed bg-cover p-40 h-auto">
+      <div className="bg-[url('https://images.unsplash.com/photo-1534768368122-aa9bbb3a8d62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBhc3RlbCUyMHBhcnR5fGVufDB8fDB8fHwy')] bg-fixed bg-cover p-40 h-auto">
         <h1 className="text-8xl text-white text-center font-mukta font-bold p-5 ">
           {userData?.user?.name} Curate your perfect Event with EventHub
         </h1>
