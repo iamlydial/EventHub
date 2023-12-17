feaa862dea8b6230d2a9cb35df3992439cdd6c2a
Nasra Hussein <nh3@live.co.uk>
2023-12-17 15:03:59 +0000

    changes to login

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "",
+  password: "y",
   database: "eventhub",
   waitForConnections: true,
   connectionLimit: 10,
